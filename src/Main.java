3a0f050823755118ac6a1cf9226185a538c2f7bf
Nhan Cao <nhancv1992@gmail.com>
2016-04-07 17:42:03 +0530

    read from file

--- src/Main.java
+++ src/Main.java
@@ -4,87 +4,35 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStreamReader;
-import java.util.List;
 
 public class Main
 {
     class Data
     {
-        private String title;
-        private Long id;
-        private Boolean children;
-        private List<Data> groups;
+        private String path;
 
-        public String getTitle()
+        public String getPath()
         {
-            return title;
+            return path;
         }
 
-        public Long getId()
+        public void setPath( String path )
         {
-            return id;
+            this.path = path;
         }
 
-        public Boolean getChildren()
+        @Override public String toString()
         {
-            return children;
-        }
-
-        public List<Data> getGroups()
-        {
-            return groups;
-        }
-
-        public void setTitle( String title )
-        {
-            this.title = title;
-        }
-
-        public void setId( Long id )
-        {
-            this.id = id;
-        }
-
-        public void setChildren( Boolean children )
-        {
-            this.children = children;
-        }
-
-        public void setGroups( List<Data> groups )
-        {
-            this.groups = groups;
-        }
-
-        public String toString()
-        {
-            return String.format( "title:%s,id:%d,children:%s,groups:%s", title, id, children, groups );
+            return "Data{" +
+                "path='" + path + '\'' +
+                '}';
         }
     }
 
     public static void main( String[] args ) throws Exception
     {
+        Data data = new Gson().fromJson( new InputStreamReader( new FileInputStream( "config.json" ) ), Data.class );
 
-        String json =
-            "{"
-                + "'title': 'Computing and Information systems',"
-                + "'id' : 1,"
-                + "'children' : 'true',"
-                + "'groups' : [{"
-                + "'title' : 'Level one CIS',"
-                + "'id' : 2,"
-                + "'children' : 'true',"
-                + "'groups' : [{"
-                + "'title' : 'Intro To Computing and Internet',"
-                + "'id' : 3,"
-                + "'children': 'false',"
-                + "'groups':[]"
-                + "}]"
-                + "}]"
-                + "}";
-//        Data data = new Gson().fromJson( new InputStreamReader( new FileInputStream( "config.json" ) ), Data.class );
-        Data data = new Gson().fromJson( json, Data.class );
-
-        // Show it.
         System.out.println( data );
 
 //        try
